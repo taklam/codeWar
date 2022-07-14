b669c3cf0c60bfd9999e3c3c5337e82be8a2e908
Tak <tak.lamtakkei@gmail.com>
2022-07-14 23:28:50 +0800

    Update mix.js

--- Strings Mix/mix.js
+++ Strings Mix/mix.js
@@ -1,3 +1,18 @@
+/*
+Question
+  s1 = "my&friend&Paul has heavy hats! &"
+  s2 = "my friend John has many many friends &"
+  mix(s1, s2) --> "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+  s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
+  s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
+  mix(s1, s2) --> "1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+  s1="Are the kids at home? aaaaa fffff"
+  s2="Yes they are here! aaaaa fffff"
+  mix(s1, s2) --> "=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh"
+*/
+
 const KEY1 = '1';
 const KEY2 = '2';
 
